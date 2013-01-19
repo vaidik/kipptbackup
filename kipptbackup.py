d30e0ee79aa6c9efd411fe657b8e9adfa02cb8d0
Vaidik Kapoor <kapoor.vaidik@gmail.com>
2013-01-19 22:43:42 +0530

    backup generation done

--- kipptbackup.py
+++ kipptbackup.py
@@ -9,6 +9,11 @@ from config import API_TOKEN, USERNAME
 
 
 class KipptBackup(object):
+    backup = {
+        'raw': None,
+        'structured': None,
+    }
+
     def __init__(self, username=None, api_token=None):
         self.username = username or USERNAME
         self.api_token = api_token or API_TOKEN
@@ -73,3 +78,52 @@ class KipptBackup(object):
                 last = True
 
             yield response
+
+    def raw_backup(self):
+        backup = dict(lists=list(), clips=list())
+
+        for l in self._get_lists():
+            backup['lists'].append(json.loads(l.text))
+
+        for c in self._get_clips():
+            backup['clips'].append(json.loads(c.text))
+
+        self.backup['raw'] = backup
+        return backup
+
+    def structured_backup(self, force=False):
+        if not self.backup['raw'] or force:
+            self.raw_backup()
+
+        structured_backup = {}
+
+        for lists in self.backup['raw']['lists']:
+            for l in lists['objects']:
+                new_list = {
+                    'title': l['title'],
+                    'is_private': l['is_private'],
+                    'description': l['description'],
+                    'clips': [],
+                }
+                structured_backup[l['resource_uri']] = new_list
+
+        for clips in self.backup['raw']['clips']:
+            for c in clips['objects']:
+                new_clip = {
+                    'is_starred': c['is_starred'],
+                    'title': c['title'],
+                    'url': c['url'],
+                    'url_domain': c['url_domain'],
+                    'list': c['list'],
+                    'is_read_later': c['is_read_later'],
+                }
+
+                try:
+                    new_clip['is_read_later'] = c['is_read_later']
+                except KeyError:
+                    print 'is_read_later error'
+
+                structured_backup[new_clip['list']]['clips'].append(new_clip)
+
+        self.backup['structured'] = structured_backup
+        return structured_backup
